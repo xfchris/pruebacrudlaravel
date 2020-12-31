c450a6d74a8c49d4750d75fe9a126661808d185f
chrisva_co <chvc2003@hotmail.com>
2020-12-30 21:37:27 -0500

    feat: se añade migracion, controlador y modelo de la tabla clientes

---
+++ database/migrations/2020_12_31_022758_create_clientes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 80);
+            $table->string('apellido', 80);
+            $table->string('telefono', 120);
+            $table->string('email', 120);
+            $table->string('direccion', 500);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
